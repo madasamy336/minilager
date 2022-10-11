b19595d5ebd7cd297d99af434e82405a93a56242
Arun233 <arun.s@6storage.com>
2022-10-11 20:48:42 +0530

    implementin routes and header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import {BrowserRouter as Router ,Routes,Route} from 'react-router-dom'
+import Header from './app/components/header/Header';
 import RentNow from './app/pages/RentNow'
+
 
 function App() {
   return (
-    <RentNow />
+    <>
+    <Router>
+       <Header/>
+      <Routes>
+        <Route exact path='/' element={<RentNow/>}/>
+      </Routes>
+    </Router>
+  
+    </>
   );
 }
 
