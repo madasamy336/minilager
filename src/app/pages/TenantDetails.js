17a09b5e80ac8594c7f665cfa6fddec7e31a3178
Arun233 <arun.s@6storage.com>
2022-10-19 19:02:17 +0530

    tenants details popup with redirection for addon

--- src/app/pages/TenantDetails.js
+++ src/app/pages/TenantDetails.js
@@ -3,13 +3,17 @@ import {Image, Modal } from "semantic-ui-react";
 import SemanticDatepicker from "react-semantic-ui-datepickers";
 import React, { useEffect, useState } from "react";
 import TenantDetailEmergengyContactAccordian from "../components/tenantDetailsAccordian/TenantDetailsAccordian";
+import { useNavigate } from "react-router-dom";
 
 export default function TenantDetails() {
+  const navigate=useNavigate()
    const [contactaccordian,SetContactaccordian]=useState([]);
    const[contactFirstName,SetContactFirstName]=useState('');
    const[contactLastName,SetContactLastName]=useState('');
    const[contactEmail,SetContactEmail]=useState('');
    const[contactPhone,SetContactPhone]=useState()
+   const [creditStatus,SetCreditStatus]=useState(false);
+
    const [creditCheckModal,SetCreditCheckModal]=useState({
     open: false,
     dimmer: undefined,  
@@ -34,6 +38,14 @@ export default function TenantDetails() {
     list.splice(index,1);
     SetContactaccordian(list)
    
+   }
+   const proceedCreditCheck=(e)=>{
+    e.preventDefault();
+    SetCreditStatus(true);
+   }
+   const navigateEsign=(e)=>{
+    e.preventDefault();
+    navigate('/preBooking/esignPayment')
    }
    useEffect(()=>{
       console.log(contactaccordian);
@@ -231,20 +243,39 @@ export default function TenantDetails() {
       <Modal
         dimmer={creditCheckModal.dimmer}
         open={creditCheckModal.open}
+        size= 'tiny'
         onClose={() => SetCreditCheckModal({ open: false })}
       >
-        <Modal.Header className='bg-success-dark text-white text-center fs-6 py-2 fw-400 position-relative'>PROMO CODES
+        <Modal.Header className='header text-success-dark text-center fw-601 fs-5 border-0 pb-1'>CREDIT CHECK</Modal.Header>
+        <Modal.Content className='mh-400 overflow-y-auto text-center pt-2'>
+        <div className="d-flex justify-content-center mb-2">
+        <Image width='250' src={ !creditStatus ?`/assets/images/creditCheck.svg`:'/assets/images/creditStatusExcellent.png'} alt='CreditCheck'/>    
+        </div>
+        {!creditStatus &&
+            <>
+              <div className="mb-3">
+                As part of the move-in process, we need to verify your credit score, would you like to proceed for a credit check?
+              </div>
+              <div className="">
+                <button onClick={() => SetCreditCheckModal({ open: false })} className="ui button bg-secondary  fs-7 fw-400 text-white px-5 mr-1">Cancel</button>
+                <button onClick={(e)=>proceedCreditCheck(e)} className="ui button bg-success-dark   fs-7 fw-400 text-white px-5">Proceed</button>
+              </div>
+            </>
+        }
+        {creditStatus && <>
+        <div className="mb-3">
+        Congratulations! You have sufficient credit score to proceed further with us
+        </div>
+        <div>
+        <button onClick={(e)=>navigateEsign(e)}  className="ui button bg-secondary  fs-7 fw-400 text-white px-5">Continue</button>
+        </div>
+      </>
+
+        }
      
-     <svg onClick={()=>SetCreditCheckModal({ open: false })} className='r-3 cursor-pointer position-absolute' xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 17.473 17.47">
-      <path id="wrong-5" d="M978.609-438.353l-2.052-2.043-4.37-4.366a1.33,1.33,0,0,1-.4-1.425,1.3,1.3,0,0,1,.833-.843,1.3,1.3,0,0,1,1.171.183,3.019,3.019,0,0,1,.353.321q3.009,3,6.009,6.01c.088.088.159.193.254.309.127-.118.217-.2.3-.281l6.156-6.156a1.332,1.332,0,0,1,1.325-.431,1.3,1.3,0,0,1,.927.828,1.3,1.3,0,0,1-.188,1.228,3.412,3.412,0,0,1-.325.35q-3,3.009-6.011,6.009a3.233,3.233,0,0,1-.317.244c.132.14.213.23.3.316q3.052,3.053,6.108,6.1a1.36,1.36,0,0,1,.441,1.387,1.305,1.305,0,0,1-2.205.564c-.59-.568-1.163-1.157-1.74-1.736l-4.487-4.491a2.068,2.068,0,0,1-.183-.248l-.142-.051a1.52,1.52,0,0,1-.191.325q-3.047,3.059-6.1,6.111a1.341,1.341,0,0,1-1.45.419,1.3,1.3,0,0,1-.851-.866,1.3,1.3,0,0,1,.235-1.19,3.215,3.215,0,0,1,.257-.274l6.034-6.033C978.386-438.167,978.484-438.245,978.609-438.353Z" transform="translate(-971.716 447.116)" fill="#fff"/>
-    </svg>
 
-
-        </Modal.Header>
-        <Modal.Content className='mh-400 overflow-y-auto'>
- 
         </Modal.Content>
       </Modal>
     </>
-  )
+  ) 
 }
