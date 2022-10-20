6af215001882a8eb5f6906f7cf9cc04cf68deeb8
Arun233 <arun.s@6storage.com>
2022-10-20 19:36:53 +0530

    mobile phone input flags and forgot password success msg and routes

--- src/app/pages/PostSignupForm.js
+++ src/app/pages/PostSignupForm.js
@@ -1,37 +1,14 @@
+
 import { useNavigate } from "react-router-dom";
 import { Dropdown, Flag, Input } from "semantic-ui-react";
-
+import countriecodes from "../components/CountryCode";
 export default function PostSignupForm() {
     const navigate = useNavigate();
+
     const ValidateSignin = (e) => {
         e.preventDefault();
-        navigate('/login')
+        navigate('/login');
     }
-    const options = [
-        { key: 'af', value: '+91', flag: 'af', text: '+91' },
-        { key: 'ax', value: 'ax', flag: 'ax', text: 'Aland Islands' },
-        { key: 'al', value: 'al', flag: 'al', text: 'Albania' },
-        { key: 'dz', value: 'dz', flag: 'dz', text: 'Algeria' },
-        { key: 'as', value: 'as', flag: 'as', text: 'American Samoa' },
-        { key: 'ad', value: 'ad', flag: 'ad', text: 'Andorra' },
-        { key: 'ao', value: 'ao', flag: 'ao', text: 'Angola' },
-        { key: 'ai', value: 'ai', flag: 'ai', text: 'Anguilla' },
-        { key: 'ag', value: 'ag', flag: 'ag', text: 'Antigua' },
-        { key: 'ar', value: 'ar', flag: 'ar', text: 'Argentina' },
-        { key: 'am', value: 'am', flag: 'am', text: 'Armenia' },
-        { key: 'aw', value: 'aw', flag: 'aw', text: 'Aruba' },
-        { key: 'au', value: 'au', flag: 'au', text: 'Australia' },
-        { key: 'at', value: 'at', flag: 'at', text: 'Austria' },
-        { key: 'az', value: 'az', flag: 'az', text: 'Azerbaijan' },
-        { key: 'bs', value: 'bs', flag: 'bs', text: 'Bahamas' },
-        { key: 'bh', value: 'bh', flag: 'bh', text: 'Bahrain' },
-        { key: 'bd', value: 'bd', flag: 'bd', text: 'Bangladesh' },
-        { key: 'bb', value: 'bb', flag: 'bb', text: 'Barbados' },
-        { key: 'by', value: 'by', flag: 'by', text: 'Belarus' },
-        { key: 'be', value: 'be', flag: 'be', text: 'Belgium' },
-        { key: 'bz', value: 'bz', flag: 'bz', text: 'Belize' },
-        { key: 'bj', value: 'bj', flag: 'bj', text: 'Benin' },
-      ]
 
     return (
         <>
@@ -88,10 +65,8 @@ export default function PostSignupForm() {
                                     <label className="d-block">Mobile Number</label>
                                     <div className="ui input w-100">
                                     <Input  type="tel" placeholder="Enter Mobile Number"
-                                            label={<Dropdown  search  options={options} />}
-                                            labelPosition='left'
-                                        
-                                        />
+                                            label={<Dropdown defaultValue='+91'  search  options={countriecodes} />}
+                                            labelPosition='left' />
                               
                                         
                                     </div>
